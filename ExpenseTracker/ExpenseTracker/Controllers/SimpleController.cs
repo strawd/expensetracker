6ddefb77e14f94569ed5497560969d09ccfa9dd9
David Straw <strawd@gmail.com>
2016-06-30 09:00:48 -0600

    Added a simple hello world controller for testing

---
+++ ExpenseTracker/ExpenseTracker/Controllers/SimpleController.cs
@@ -0,0 +1,16 @@
+﻿// Copyright 2016 David Straw
+
+using System.Web.Http;
+using Microsoft.Azure.Mobile.Server.Config;
+
+namespace ExpenseTracker.Controllers
+{
+    [MobileAppController]
+    public class SimpleController : ApiController
+    {
+        public object Get()
+        {
+            return "Hello World";
+        }
+    }
+}
